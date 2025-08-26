678eaa6d0ace47b649fa501ca6a69f0a99f84ed5
rhdzmota <code@rhdzmota.com>
2025-08-26 11:58:54 -0600

    FRD-2 Update setup file to work with new layout implementation

--- setup.py
+++ setup.py
@@ -1,10 +1,47 @@
-from setuptools import find_packages, setup
+import os
+import json
+from typing import Dict, List
+from setuptools import setup, find_namespace_packages
+
+
+CODEBASE_PATH = os.environ.get(
+    "CODEBASE_PATH",
+    default=os.path.join("src", "main"),
+)
+
+with open("requirements.txt", "r") as file:
+    requirements = [line for line in file.read().splitlines() if line and not line.startswith("#")]
+
+version_filepath = os.path.join(CODEBASE_PATH, "fred", "proj", "gpt_oss_vulnerabilities", "version")
+with open(version_filepath, "r") as file:
+    version = file.read().strip()
+
+with open("README.md") as file:
+    readme = file.read()
 
 setup(
-    name='src',
-    packages=find_packages(),
-    version='0.1.0',
-    description='',
-    author='Fahera Research, Education, and Development',
-    license='',
+    name="fred-kaggle-gpt-oss-vulnerabilities",
+    version=version,
+    description="FRED Kaggle Challenges: GPT OSS Vulnerabilities",
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    url="https://fred.fahera.mx",
+    author="Fahera Research, Education, and Development",
+    author_email="fred@fahera.mx",
+    packages=find_namespace_packages(where=CODEBASE_PATH),
+    package_dir={
+        "": CODEBASE_PATH
+    },
+    package_data={
+        "": [
+            version_filepath,
+        ]
+    },
+    entry_points={
+        "console_scripts": [
+            "fred.gpt_oss_vulnerabilities=fred.proj.gpt_oss_vulnerabilities.cli:CLI.cli_exec",
+        ]
+    },
+    include_package_data=True,
+    python_requires=">=3.11.0"
 )
