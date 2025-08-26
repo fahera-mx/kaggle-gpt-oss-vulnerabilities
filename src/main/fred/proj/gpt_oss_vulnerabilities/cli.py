863a05aa081460f4e52d32a2d6d35e1ccb5e3f90
rhdzmota <code@rhdzmota.com>
2025-08-26 13:27:04 -0600

    FRD-6 Load dotenv automatically post-init in CLI class

--- src/main/fred/proj/gpt_oss_vulnerabilities/cli.py
+++ src/main/fred/proj/gpt_oss_vulnerabilities/cli.py
@@ -10,6 +10,11 @@ class CLI:
     start_counter: float = field(default_factory=time.perf_counter)
     start_ts: str = field(default_factory=dt.datetime.utcnow().isoformat)
 
+    def __post_init__(self):
+        from dotenv import load_dotenv
+
+        load_dotenv()
+
     @classmethod
     def cli_exec(cls, *args, **kwargs):
         import fire
@@ -31,3 +36,18 @@ class CLI:
 
     def hello(self, name: Optional[str] = None) -> str:
         return f"Hello, {name or 'World'}!"
+    
+    def run_experiment(
+            self,
+            size: int,
+            experiment_params_filepath: str,
+            output_dirpath: Optional[str] = None,
+    ):
+        from fred.proj.gpt_oss_vulnerabilities.experiment import Experiment
+
+        output_dirpath = output_dirpath or os.path.join(
+            os.path.dirname(experiment_params_filepath),
+            "experiment-output"
+        )
+        experiment = Experiment.from_csv(experiment_params_filepath)
+        experiment.run(output_dirname=output_dirpath, sample_size=size)
